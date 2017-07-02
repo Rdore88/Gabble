28bf35aa272ac51fa4a7f5e098068b2ccdfecfea
Robby Dore <rdore88@gmail.com>
2017-07-02 16:51:20 -0400

    Redoing database work

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const expressSession = require('express-session');
 const expressValidator = require('express-validator');
+const homeRouter = require('./routes/home.js');
+const likeRouter = require('./routes/like.js');
+const messageRouter = require('./routes/message.js');
+const userRouter = require('./routes/user.js');
+
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './public/views');
@@ -12,6 +17,11 @@ app.set('view engine', 'mustache');
 app.use(bodyParser());
 app.use(express.static('public'));
 app.use(expressValidator());
+
+app.use('/user', userRouter);
+app.use('/message', messageRouter);
+app.use('/like', likeRouter);
+app.use('/', homeRouter);
 
 
 app.listen(3000, function(){
